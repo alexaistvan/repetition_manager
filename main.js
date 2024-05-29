25941598734519ac5c088067c239640a3526e9f3
alexaistvan <alexa.istvan93@gmail.com>
2024-05-29 14:18:29 +0200

    v3

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 function parseTvSchedule() {
   let tvScheduleInput = document.getElementById("tvScheduleInput").value;
   let programs = tvScheduleInput.split(/\s(?=\d+\.\d+)/);
+  let specialChars = /[ｦｧｨｩｰｱｲｳｴｵｶｷｸ]/;
+
   let programObjects = programs.map((program) => {
     let [start, title] = program.split(/ (.+)/);
+
     title = title.replace(/ - /g, " – ");
     if (/:\s[a-zA-Z]/.test(title)) {
       title = title.replace(
@@ -10,18 +13,35 @@ function parseTvSchedule() {
         (_, letter) => `: ${letter.toUpperCase()}`
       );
     }
+
+    // a címben van e speciális karakter
+    let match = title.match(specialChars);
+    if (match) {
+      title = title.replace(specialChars, ""); // a speci karakter eltávolítása a címből
+      title += ` ${match[0]}`; // majd hozzá adása a cím végére.
+    }
+
     return { start, title };
   });
 
-  let cleanedProgramObjects = handleRepeatCounter(programObjects);
+  let cleanedProgramObjects = handleRepeatCounter(programObjects, specialChars);
 
   displayTvSchedule(cleanedProgramObjects);
 }
 
-function handleRepeatCounter(programObjects) {
+function handleRepeatCounter(programObjects, specialChars) {
   let repeatCounter = 1;
   for (let i = 0; i < programObjects.length - 1; i++) {
-    if (programObjects[i].title === programObjects[i + 1].title) {
+    let titleWithoutSpecialChars = programObjects[i].title.replace(
+      specialChars,
+      ""
+    );
+    let nextTitleWithoutSpecialChars = programObjects[i + 1].title.replace(
+      specialChars,
+      ""
+    );
+
+    if (titleWithoutSpecialChars === nextTitleWithoutSpecialChars) {
       repeatCounter++;
       if (repeatCounter > 1) {
         programObjects.splice(i + 1, 1);
@@ -29,9 +49,30 @@ function handleRepeatCounter(programObjects) {
       }
     } else {
       if (repeatCounter > 1) {
-        programObjects[i].title += ` (${repeatCounter} rész)`;
+        let specialCharMatch = programObjects[i].title.match(specialChars);
+        if (specialCharMatch) {
+          programObjects[i].title = programObjects[i].title.replace(
+            specialChars,
+            ""
+          ); // speci karakter eltávolítása a címből
+          programObjects[
+            i
+          ].title += ` (${repeatCounter} rész) ${specialCharMatch[0]}`; // zárójel utáni karakter hozzá adása
+        } else {
+          programObjects[i].title += ` (${repeatCounter} rész)`;
+        }
       }
       repeatCounter = 1;
+    }
+  }
+  if (repeatCounter > 1) {
+    let lastProgram = programObjects[programObjects.length - 1];
+    let specialCharMatch = lastProgram.title.match(specialChars);
+    if (specialCharMatch) {
+      lastProgram.title = lastProgram.title.replace(specialChars, "");
+      lastProgram.title += ` (${repeatCounter} rész)${specialCharMatch[0]}`;
+    } else {
+      lastProgram.title += ` (${repeatCounter} rész)`;
     }
   }
   return programObjects;
@@ -47,9 +88,22 @@ function displayTvSchedule(programObjects) {
   let outputText2 = "";
   for (let show of programObjects) {
     outputText += `${show.start} ${show.title} `;
-    let titleWithoutParentheses = show.title.replace(/\s\(\d+ rész\)/, "");
+    // zárójel eltávolítása v2ben
+    let titleWithoutParentheses = show.title.replace(
+      /\s\(\d+ rész\)[ｦｧｨｩｰｱｲｳｴｵｶｷｸ]?/,
+      ""
+    );
     outputText2 += `${show.start} ${titleWithoutParentheses} `;
   }
   outputDiv.textContent = outputText.trim();
   shorterOutput.textContent = outputText2.trim();
 }
+
+//refresh
+let refreshBtn = document.getElementById("btnRefresh");
+
+function handleClick() {
+  window.location.reload();
+}
+
+refreshBtn.addEventListener("click", handleClick);
